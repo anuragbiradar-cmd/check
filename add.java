c4dce8794f42748c486d972afda4ca469ef9f426
anuragbiradar-cmd <82156833+anuragbiradar-cmd@users.noreply.github.com>
2021-12-15 12:50:39 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String[]args)
+{
+String fi="hello";
+String nes="";
+for(int i=0;i<fi.length();i++)
+{
+nes=fi.charAt(i)+nes;
+
+}
+System.out.println(nes);
+}
+}
